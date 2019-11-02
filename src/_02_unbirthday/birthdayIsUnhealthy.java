4b422302c47354f1ccd82cf3acb282b7aa2c567b
League Student <league@iMac-11.attlocal.net>
2019-11-02 12:59:33 -0700

    :eggplant: mod 1

---
+++ src/_02_unbirthday/birthdayIsUnhealthy.java
@@ -0,0 +1,23 @@
+package _02_unbirthday;
+
+import javax.swing.JOptionPane;
+
+public class birthdayIsUnhealthy {
+
+	public static void main(String[] args) {
+		
+		String e = JOptionPane.showInputDialog("when is yoru birthday? put like this: mm/dd");
+		if(e == "11/02") {
+			JOptionPane.showMessageDialog(null, "happy bithday chunky!");
+		}
+		
+		else {
+			JOptionPane.showMessageDialog(null, "it is illegal to not be your birthday today!");
+		}
+		
+		
+		
+		//eeeeeeeeeeeee
+	}
+
+}
