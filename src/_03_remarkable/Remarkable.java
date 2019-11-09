6da56ce9765b7642dc4c65c226e1d513bbd45820
League Student <league@iMac-11.attlocal.net>
2019-11-09 12:58:38 -0800

    :eggplant:
    :eggplant: yes pizza

--- src/_03_remarkable/Remarkable.java
+++ src/_03_remarkable/Remarkable.java
@@ -8,11 +8,20 @@ public class Remarkable {
 public static void main(String[] args) {
 
 		// 1. Save something remarkable about each person in a variable.
-
+		String yorDad = "chonky";
+		String yorMom = "not chonky";
 		// 2. Ask the user to enter a name. Store their answer in a variable.
-
+		String stinky = JOptionPane.showInputDialog("enter a name? (yorMom or yorDad)");
 		// 3. In a pop-up, tell the user what is remarkable about that person. 
+			if (stinky != "yorDad") {
+				JOptionPane.showMessageDialog(null, "youDad is " + yorDad);
+			}
+			if (stinky != "yorMom") {
+				JOptionPane.showMessageDialog(null, "yorMom is " + yorMom);
+			}
+			
 
+//eeeeeeeeeeeeee
 	}
 }
 
--- src/_06_pizza/pizza/pizza.pde
+++ src/_06_pizza/pizza/pizza.pde
@@ -1,7 +1,15 @@
 
 void setup() {
-    
+    size(500,500);
+    fill(100,50,40);
+    ellipse(250,250,300,300);
+    fill(200,0,0);
+    ellipse(250,250,275,275);
+    fill(200,185,0);
+    ellipse(250,250,250,250);
 }
 void draw() {
-    
+if(mousePressed==true){
+  ellipse(
+}
 }
